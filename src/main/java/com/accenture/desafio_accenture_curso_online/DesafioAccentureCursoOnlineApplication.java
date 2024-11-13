759c1824e9e35576b2ab78586ac0693ed331bb0b
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-11-13 11:40:38 -0300

    Projeto criado

---
+++ src/main/java/com/accenture/desafio_accenture_curso_online/DesafioAccentureCursoOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.desafio_accenture_curso_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAccentureCursoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAccentureCursoOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/desafio_accenture_curso_online/DesafioAccentureCursoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.desafio_accenture_curso_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAccentureCursoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
